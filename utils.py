d5a3509a8c520044bb1cdee0c6f5af3f471700f2
jjrylearn <joshuaryanlearn@gmail.com>
2019-02-10 01:50:33 -0500

    added util functions for getting metadata and converting to json

--- utils.py
+++ utils.py
@@ -1,10 +1,19 @@
 import datetime
+import json
 import os
 import requests
 import shutil
+import tempfile
+import xmltodict
+
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 HELIOVIEWER_API_PREFIX = "https://api.helioviewer.org/v2/getJP2Image/?"
+
+HELIOVIEWER_API_PREFIX = "https://api.helioviewer.org/v2/{}/?"
+GET_IMAGE_PREFIX = HELIOVIEWER_API_PREFIX.format("getJP2Image")
+GET_CLOSEST_IMAGE_PREFIX = HELIOVIEWER_API_PREFIX.format("getClosestImage")
+GET_METADATA_PREFIX = HELIOVIEWER_API_PREFIX.format("getJP2Header")
 
 
 def build_helioview_url(params):
@@ -25,7 +34,7 @@ def build_helioview_url(params):
         + "Z"
     )  # Z added to indicate "Zulu" (UTC) timezone
 
-    source_id = str(params["sourceId"])
+    source_id = str(params["source_id"])
 
     return "date={}&sourceId={}".format(date_string, source_id)
 
@@ -37,18 +46,63 @@ def get_helioview_image(params):
     :params: Query parameters
     """
     url = build_helioview_url(params)
-    r = requests.get(HELIOVIEWER_API_PREFIX + url, stream=True)
+    r = requests.get(GET_IMAGE_PREFIX + url, stream=True)
     if r.status_code == 200:
         r.raw.decode_content = True
         return r.raw
 
 
-def download_file(filename, data):
+def get_image_id(params):
+    """
+    retrieve image id from helioviewer URL
+
+    :params: Query parameters
+    """
+    url = build_helioview_url(params)
+    r = requests.get(GET_CLOSEST_IMAGE_PREFIX + url, stream=True)
+    if r.status_code == 200:
+        return r.json()["id"]
+
+
+def get_image_metadata(id):
+    """
+    retrieve image metadata from image ID
+
+    :id: unique ID of jp2 image (retrieved from get_image_id)
+    """
+    url = GET_METADATA_PREFIX + "id=" + id
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        r.raw.decode_content = True
+        return json.dumps(xmltodict.parse(r.text))
+
+
+def download_file(params):
     """
     Save raw image data as file
 
-    :filename: name to save file data as (excluding file extension)
-    :data: raw image data to save
+    :params: Query parameters
     """
-    with open(os.path.join(DIR, filename + ".jp2"), "wb") as f:
-        shutil.copyfileobj(data, f)
+    temp = tempfile.NamedTemporaryFile()
+    try:
+        raw_data = get_helioview_image(params)
+        shutil.copyfileobj(raw_data, f)
+    finally:
+        temp.close()
+    return temp.name
+
+
+def remove_file(filename):
+    os.remove(filename)
+
+
+def build_query_dict(year, month, day, hour, minute, second, source_id):
+    return {
+        "year": year,
+        "month": month,
+        "day": day,
+        "hour": hour,
+        "minute": minute,
+        "second": second,
+        "source_id": source_id,
+    }
