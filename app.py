a23f24ca93fde9f18a7de3e062903007c91ac247
joetats <tatuskojc@gmail.com>
2019-02-09 23:09:07 -0500

    add /sun GET route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import datetime
 
 app = Flask(__name__)
 
@@ -6,6 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/sun')
+def sun():
+    start_datetime = datetime.datetime(*request.args.get('start_date'), *request.args.get('start_time'))
+    end_datetime = datetime.datetime(*request.args.get('end_date'), *request.args.get('end_time'))
+    resolution = request.args.get('resolution')
+    wavelength = request.args.get('wavelength')
+
+    return 'Something was done here.'
 
 if __name__ == '__main__':
     app.run()
